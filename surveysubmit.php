bd39aabe417cb96621a77ef1bb5bccf5ec9b7991
Randeeno <ejames1114@gmail.com>
2022-03-14 11:59:11 -0500

    Update surveysubmit.php

--- surveysubmit.php
+++ surveysubmit.php
@@ -1,1 +1,10 @@
+<?php include '/etc/capstone/database.php';
+        // DB connection
+        $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
 
+        // Check connection
+        if ($mysqli -> connect_errno) {
+          echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+          exit();
+        }
+?> 
