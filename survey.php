4a4e10dc111b0d6379e9997fa5ca51cd15979880
Randeeno <ejames1114@gmail.com>
2022-03-13 21:13:23 -0500

    Update and rename survey.html to survey.php

--- survey.php
+++ survey.php
@@ -6,6 +6,16 @@
     </head>
 
     <body>
+        <?php include '/etc/capstone/database.php';
+        // DB connection
+        $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
+
+        // Check connection
+        if ($mysqli -> connect_errno) {
+          echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+          exit();
+        }
+        ?> 
         <section class="survey-body">
             <h1>Website Survey</h1>
             <form name="submitForm" action="/thankYou.html" method="post" required>
