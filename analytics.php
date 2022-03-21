249dd7df2640290a02f6ef50ad49d24b1f087ef1
Randeeno <ejames1114@gmail.com>
2022-03-21 14:46:54 -0500

    Update analytics.php

--- analytics.php
+++ analytics.php
@@ -6,7 +6,7 @@ Capstone IT4970
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <link rel="stylesheet" href="indexSS.css">
+        <link rel="stylesheet" href="analyticsSS.css">
         <title>QR Code Analytics</title>
         <meta charset="utf-8">
 
@@ -69,203 +69,101 @@ Capstone IT4970
                 <th>Number of Scans</th>
             </tr>
             <tr>
-                <td>12 and under</td>
-                <td>
-                    <?php include '/etc/capstone/database.php';
-                    // DB connection
-                    $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
-
-                    // Check connection
-                    if ($mysqli -> connect_errno) {
-                        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-                        exit();
-                    }
-            
-                    $sql12 = "SELECT * FROM surveydata WHERE age BETWEEN 0 AND 12";
-                    if ($result12 = mysqli_query($mysqli, $sql12)) {
-  
-                    // Return the number of rows in result set
-                    $rowcount12 = mysqli_num_rows( $result12 );
-      
-                    // Display result
-                    echo "$rowcount12"
-                    }
-                    ?>
-                </td>
-            </tr>
-            <tr>
                 <td>13-17</td>
                 <td>
-                    <?php include '/etc/capstone/database.php';
-                    // DB connection
-                    $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
-
-                    // Check connection
-                    if ($mysqli -> connect_errno) {
-                        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-                        exit();
-                    }
-            
-                    $sql13 = "SELECT * FROM surveydata WHERE age BETWEEN 13 AND 17";
-                    if ($result13 = mysqli_query($mysqli, $sql13)) {
-  
-                    // Return the number of rows in result set
+                <?php include '/etc/capstone/database.php';
+                // DB Con
+                $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
+                $sql13 = "SELECT * FROM surveydata WHERE age BETWEEN 13 AND 17";
+                if ($result13 = mysqli_query($mysqli, $sql13)){
                     $rowcount13 = mysqli_num_rows( $result13 );
-      
-                    // Display result
-                    echo "$rowcount13"
-                    }
-                    ?>
+                    echo "$rowcount13";
+                }
+                ?>
                 </td>
             </tr>
             <tr>
-                <td>18-24</td>
+                <td>18-22</td>
                 <td>
-                    <?php include '/etc/capstone/database.php';
-                    // DB connection
-                    $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
-
-                    // Check connection
-                    if ($mysqli -> connect_errno) {
-                        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-                        exit();
-                    }
-            
-                    $sql18 = "SELECT * FROM surveydata WHERE age BETWEEN 18 AND 24";
-                    if ($result18 = mysqli_query($mysqli, $sql18)) {
-  
-                    // Return the number of rows in result set
+                <?php include '/etc/capstone/database.php';
+                // DB Con
+                $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
+                $sql18 = "SELECT * FROM surveydata WHERE age BETWEEN 18 AND 22";
+                if ($result18 = mysqli_query($mysqli, $sql18)){
                     $rowcount18 = mysqli_num_rows( $result18 );
-      
-                    // Display result
-                    echo "$rowcount18"
-                    }
-                    ?>
+                    echo "$rowcount18";
+                }
+                ?>
                 </td>
             </tr>
             <tr>
-                <td>25-34</td>
+                <td>23-34</td>
                 <td>
-                    <?php include '/etc/capstone/database.php';
-                    // DB connection
-                    $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
-
-                    // Check connection
-                    if ($mysqli -> connect_errno) {
-                        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-                        exit();
-                    }
-            
-                    $sql25 = "SELECT * FROM surveydata WHERE age BETWEEN 25 AND 34";
-                    if ($result25 = mysqli_query($mysqli, $sql25)) {
-  
-                    // Return the number of rows in result set
-                    $rowcount25 = mysqli_num_rows( $result25 );
-      
-                    // Display result
-                    echo "$rowcount25"
-                    }
-                    ?>
+                <?php include '/etc/capstone/database.php';
+                // DB Con
+                $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
+                $sql23 = "SELECT * FROM surveydata WHERE age BETWEEN 23 AND 34";
+                if ($result23 = mysqli_query($mysqli, $sql23)){
+                    $rowcount23 = mysqli_num_rows( $result23 );
+                    echo "$rowcount23";
+                }
+                ?>
                 </td>
             </tr>
             <tr>
                 <td>35-44</td>
                 <td>
-                    <?php include '/etc/capstone/database.php';
-                    // DB connection
-                    $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
-
-                    // Check connection
-                    if ($mysqli -> connect_errno) {
-                        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-                        exit();
-                    }
-            
-                    $sql35 = "SELECT * FROM surveydata WHERE age BETWEEN 35 AND 44";
-                    if ($result35 = mysqli_query($mysqli, $sql35)) {
-  
-                    // Return the number of rows in result set
+                <?php include '/etc/capstone/database.php';
+                // DB Con
+                $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
+                $sql35 = "SELECT * FROM surveydata WHERE age BETWEEN 35 AND 44";
+                if ($result35 = mysqli_query($mysqli, $sql35)){
                     $rowcount35 = mysqli_num_rows( $result35 );
-      
-                    // Display result
-                    echo "$rowcount35"
-                    }
-                    ?>
+                    echo "$rowcount35";
+                }
+                ?>
                 </td>
             </tr>
             <tr>
                 <td>45-54</td>
                 <td>
-                    <?php include '/etc/capstone/database.php';
-                    // DB connection
-                    $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
-
-                    // Check connection
-                    if ($mysqli -> connect_errno) {
-                        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-                        exit();
-                    }
-            
-                    $sql45 = "SELECT * FROM surveydata WHERE age BETWEEN 45 AND 54";
-                    if ($result45 = mysqli_query($mysqli, $sql45)) {
-  
-                    // Return the number of rows in result set
+                <?php include '/etc/capstone/database.php';
+                // DB Con
+                $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
+                $sql45 = "SELECT * FROM surveydata WHERE age BETWEEN 45 AND 54";
+                if ($result45 = mysqli_query($mysqli, $sql45)){
                     $rowcount45 = mysqli_num_rows( $result45 );
-      
-                    // Display result
-                    echo "$rowcount45"
-                    }
-                    ?>
+                    echo "$rowcount45";
+                }
+                ?>
                 </td>
             </tr>
             <tr>
                 <td>55-64</td>
                 <td>
-                    <?php include '/etc/capstone/database.php';
-                    // DB connection
-                    $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
-
-                    // Check connection
-                    if ($mysqli -> connect_errno) {
-                        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-                        exit();
-                    }
-            
-                    $sql55 = "SELECT * FROM surveydata WHERE age BETWEEN 55 AND 64";
-                    if ($result55 = mysqli_query($mysqli, $sql55) {
-  
-                    // Return the number of rows in result set
+                <?php include '/etc/capstone/database.php';
+                // DB Con
+                $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
+                $sql55 = "SELECT * FROM surveydata WHERE age BETWEEN 55 AND 64";
+                if ($result55 = mysqli_query($mysqli, $sql55)){
                     $rowcount55 = mysqli_num_rows( $result55 );
-      
-                    // Display result
-                    echo "$rowcount55"
-                    }
-                    ?>
+                    echo "$rowcount55";
+                }
+                ?>
                 </td>
             </tr>
             <tr>
                 <td>65 and older</td>
                 <td>
-                    <?php include '/etc/capstone/database.php';
-                    // DB connection
-                    $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
-
-                    // Check connection
-                    if ($mysqli -> connect_errno) {
-                        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-                        exit();
-                    }
-            
-                    $sql65 = "SELECT * FROM surveydata WHERE age BETWEEN 65 AND 100";
-                    if ($result65 = mysqli_query($mysqli, $sql65)) {
-  
-                    // Return the number of rows in result set
+                <?php include '/etc/capstone/database.php';
+                // DB Con
+                $mysqli = new mysqli($HOST,$USER,$DB_PSWD,$DB_NAME);
+                $sql65 = "SELECT * FROM surveydata WHERE age BETWEEN 65 AND 100";
+                if ($result65 = mysqli_query($mysqli, $sql65)){
                     $rowcount65 = mysqli_num_rows( $result65 );
-      
-                    // Display result
-                    echo "$rowcount65"
-                    }
-                    ?>
+                    echo "$rowcount65";
+                }
+                ?>
                 </td>
             </tr>
         </table>
